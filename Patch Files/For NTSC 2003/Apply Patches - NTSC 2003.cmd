fbb6d380a1ca7430d6b3a20ad836632a5d7bf6ca
SomeGTAVCCodes <bluegymleader@gmail.com>
2025-08-04 17:20:51 +0100

    Delete Patch Files directory

--- Patch Files/For NTSC 2003/Apply Patches - NTSC 2003.cmd
+++
@@ -1,9 +0,0 @@
-bspatch SLUS_205.52 new.elf patch.elf
-
-bspatch american.gxt new.gxt patch.gxt
-
-bspatch ioprp255.img new.img patch.img
-
-bspatch sampman2.irx beta.irx patch.irx
-
-bspatch main.scm TKBS.scm patch.scm
